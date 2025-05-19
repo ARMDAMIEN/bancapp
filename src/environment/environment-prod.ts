9aac28ecb9547eee26d4251a584fab1901b3accb
mounir <armourdom.damien.pro@gmail.com>
2025-05-19 16:20:13 +0200

    fix urls in authservice

--- src/environment/environment-prod.ts
+++ src/environment/environment-prod.ts
@@ -1,4 +1,4 @@
-// src/environments/environment.prod.ts
+// src/environment/environment-prod.ts
 export const environment = {
   production: true,
   finnhubApiKey: process.env['FINNHUB_API_KEY'],
--- src/services/authService.ts
+++ src/services/authService.ts
@@ -2,6 +2,7 @@ import { Inject, Injectable, PLATFORM_ID, TransferState, makeStateKey } from "@a
 import { HttpClient } from "@angular/common/http";
 import { Observable } from "rxjs";
 import { isPlatformServer } from "@angular/common";
+import { environment } from "../environment/environment-prod";
 
 const TOKEN_KEY = makeStateKey<string>('authToken');
 
@@ -10,44 +11,42 @@ const TOKEN_KEY = makeStateKey<string>('authToken');
 })
 export class AuthService {
 
-  constructor(private http: HttpClient, private transferState : TransferState, @Inject(PLATFORM_ID) private platformId: Object){ }
+  private apiUrl = environment.apiUrl;
+
+  constructor(private http: HttpClient, private transferState: TransferState, @Inject(PLATFORM_ID) private platformId: Object) { }
 
   login(creditentials: { username: string, password: string }): Observable<any> {
-    return this.http.post('http://localhost:8080/loginJWT', creditentials);
+    return this.http.post(`${this.apiUrl}/loginJWT`, creditentials);
   }
 
   initiateLogin() {
-    console.log("test oauth2")
-    window.location.href = 'http://localhost:8080/oauth2/redirect?provider=google';
+    window.location.href = `${this.apiUrl}/oauth2/redirect?provider=google`;
   }
 
   signUp(signUpData: { email: string; password: string }): Observable<any> {
-    console.log('in signUp with : ' + signUpData.email);
-    return this.http.post('http://localhost:8080/createUser', signUpData, {responseType : 'json'});
+    return this.http.post(`${this.apiUrl}/createUser`, signUpData, {responseType: 'json'});
   }
 
   getAuth(): Observable<any> {
-    return this.http.get('http://localhost:8080/getAuthenticated');
+    return this.http.get(`${this.apiUrl}/getAuthenticated`);
   }
 
   getAuthOauth2(): Observable<any> {
-    return this.http.get('http://localhost:8080/getOauth2Token', {
+    return this.http.get(`${this.apiUrl}/getOauth2Token`, {
       withCredentials: true
     });
   }
 
   initiateLoginMicrosfoft() {
-    console.log("test oauth2 microsoft")
-    window.location.href = 'http://localhost:8080/oauth2/redirect?provider=microsoft';
+    window.location.href = `${this.apiUrl}/oauth2/redirect?provider=microsoft`;
   }
 
-  forgotPassword(username : string): Observable<any>{
-    return this.http.get('http://localhost:8080/forgotPassword?username='+username, {responseType : 'json'});
+  forgotPassword(username: string): Observable<any> {
+    return this.http.get(`${this.apiUrl}/forgotPassword?username=${username}`, {responseType: 'json'});
   }
 
   changePassword(changeData: { email: string; password: string }): Observable<any> {
-    console.log('in changePassword with : ' + changeData.email);
-    return this.http.post('http://localhost:8080/changePassword', changeData, {responseType : 'json'});
+    return this.http.post(`${this.apiUrl}/changePassword`, changeData, {responseType: 'json'});
   }
 
   getToken(): string | null {
