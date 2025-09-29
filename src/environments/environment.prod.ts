a2252c5ed86aa431fb8764e461e57e00d6226acb
mounir <armourdom.damien.pro@gmail.com>
2025-09-29 13:23:19 +0200

    add environment back url

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -2,5 +2,5 @@
 export const environment = {
   production: true,
   finnhubApiKey: 'd0fieg1r01qsv9ehn9kgd0fieg1r01qsv9ehn9l0',
-  apiUrl: 'https://tradingcryptobot-be-ae06e674d043.herokuapp.com'
+  apiUrl: 'https://bancapp-back-2a2c736c384d.herokuapp.com/'
 };
